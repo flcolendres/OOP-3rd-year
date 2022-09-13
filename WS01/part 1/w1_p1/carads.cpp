eb0069f1b5b59afd5f6e1fb4b88856f175db2e44
Francis Colendres <flcolendres@myseneca.ca>
2022-09-12 20:03:17 -0400

    continue at home

--- WS01/part 1/w1_p1/carads.cpp
+++ WS01/part 1/w1_p1/carads.cpp
@@ -42,3 +42,26 @@ istream& Cars::read(std::istream& is)
    }
    return is;
 }
+
+std::ostream& sdds::Cars::display(bool reset)
+{
+   static int counter{};
+   cout.setf(ios::left);
+   cout.width(2);
+   cout << counter++;
+   cout.width(10);
+   cout << car_brand;
+   cout.width(15);
+   cout << car_model;
+   cout.unsetf(ios::left);
+   cout.width(4);
+   cout << car_year;
+   cout.setf(ios::left);
+   cout.width(12);
+   cout.precision(2);
+   cout << car_price + (car_price * g_taxrate);
+
+   return cout;
+}
+//COUNTER.Brand | Model | Year | Price w / Tax | Special Price
+
--- WS01/part 1/w1_p1/carads.h
+++ WS01/part 1/w1_p1/carads.h
@@ -1,9 +1,22 @@
 #ifndef SDDS_CARADS_H
 #define SDDS_CARADS_H
+#include <iostream>
 extern double g_taxrate, g_discount;
 namespace sdds 
 {
+   class Cars 
+   {
+      char car_brand[11]{};
+      char car_model[16]{};
+      unsigned int car_year{};
+      double car_price{};
+      char car_status{};
+      bool car_promo{};
+   public:
+      Cars();
+      std::istream& read(std::istream& is);
+      std::ostream& display(bool reset);
+   };
    void listArgs(int argc, char* argv[]);
 }
-
 #endif
