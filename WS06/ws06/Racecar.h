a21d064f0e8125e1467533637565b0ccc556f67c
Francis Colendres <flcolendres@myseneca.ca>
2022-11-05 20:37:22 -0400

    starting ws2

---
+++ WS06/ws06/Racecar.h
@@ -0,0 +1,13 @@
+/*
+* Francis Lloyd Colendres
+* flcolendres@myseneca.ca
+* 145498200
+* Oct. 29, 2022
+* I have done all the coding by myself and only copied the code that
+* my professor provided to complete my workshops and assignments.
+*/
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+
